8b59407ce2fb2b1aeda64e2ec8e34211040d528a
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-12 19:07:40 +0200

    Javascript e html completati

--- js/script.js
+++ js/script.js
@@ -13,7 +13,20 @@ while (yourSurname !== "") {
   for (var i = 0; i < surnameList.length; i++) {
     if (surnameList[i] === yourSurname) {
       console.log("Posizione cognome aggiunto in lista ordinata: " + (i + 1));
+      document.getElementById("surname-added").innerHTML +=
+        "Nuovo cognome aggiunto: " +
+        yourSurname +
+        ". Posizione cognome aggiunto in lista ordinata:  " +
+        (i + 1) +
+        "<br>";
     }
   }
   yourSurname = prompt("Qual è il tuo cognome?");
+}
+
+for (var i = 0; i < surnameList.length; i++) {
+  var list_item = surnameList[i];
+  var before_content = document.getElementById("list-updated").innerHTML;
+  document.getElementById("list-updated").innerHTML =
+    before_content + "<li>" + list_item + "</li>";
 }
